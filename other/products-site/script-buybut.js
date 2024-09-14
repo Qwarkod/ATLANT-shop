0ef1d6f1226f96e89aed128644858cf2479111bb
Andriy <128635256+Qwarkod@users.noreply.github.com>
2024-09-14 16:14:53 +0300

    Delete other/products-site directory

--- other/products-site/script-buybut.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('buy').addEventListener('click', function(e) {
-    e.preventDefault(); // Скасувати стандартну поведінку форми
-    window.location.href = 'https://stream-lizard-golf.glitch.me/'; // Перенаправлення на інший сайт
-    console.log('YES');
-});
