a70ddc52eb31947c41163cf14bc802a712e12c44
Andriy <128635256+Qwarkod@users.noreply.github.com>
2024-09-14 14:58:21 +0300

    Update script-buybut.js

--- other/products-site/script-buybut.js
+++ other/products-site/script-buybut.js
@@ -1,5 +1,5 @@
 document.getElementById('buy').addEventListener('click', function(e) {
     e.preventDefault(); // Скасувати стандартну поведінку форми
-    window.location.href = 'http://localhost:3000'; // Перенаправлення на інший сайт
+    window.location.href = 'https://stream-lizard-golf.glitch.me/'; // Перенаправлення на інший сайт
     console.log('YES');
 });
